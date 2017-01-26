8c722791a56e1b826e94e5cf8ad23375a0110aa7
ravisharmaa <bastola.ravi@gmail.com>
2017-01-26 16:55:11 +0545

    srn:trying_customization

--- app/Library/MyLibrary.php
+++ app/Library/MyLibrary.php
@@ -1,7 +1,8 @@
 <?php
 /**
+ *
  * Created by PhpStorm.
- * User: amit
+ * User: Ravi
  * Date: 2017-01-26
  * Time: 3:52 PM
  */
@@ -24,11 +25,39 @@ class MyLibrary extends Bootstrap
 
     protected static function open()
     {
-
+        //this opens the form when called
     }
 
-    public static function text(array $params)
+    public static function text(array $params, $extra_values=[])
     {
         $html = "<input type='text' name='$params[0]'>";
+        return $html;
     }
+
+    protected static function close()
+    {
+        //this closes the form
+    }
+
+    protected static function textarea()
+    {
+        //function for textarea
+    }
+
+    protected static function radio()
+    {
+        //this makes the radio input
+    }
+
+    protected static function file()
+    {
+        //this makes the input field for files
+    }
+
+    protected static function select()
+    {
+        //this generates the input field for the select box
+    }
+
+
 }
